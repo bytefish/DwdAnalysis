8d0d13a238d4265db1282448a38386a19f047051
Philipp Wagner <pwag86@googlemail.com>
2023-09-21 08:54:36 +0200

    Only use September Values. Remove MERGE Statements

--- DwdAnalysis/DwdAnalysis/Program.cs
+++ DwdAnalysis/DwdAnalysis/Program.cs
@@ -4,10 +4,11 @@ using FluentFTP;
 using Microsoft.Data.SqlClient;
 using Microsoft.Data.SqlClient.Server;
 using System.Data;
+using System.Globalization;
 using System.IO.Compression;
 using System.Text.RegularExpressions;
 
-namespace AsyncMainDemo
+namespace DwdAnalysis
 {
     /// <summary>
     /// Extensions for <see cref="SqlDataRecord"/>.
@@ -75,7 +76,14 @@ namespace AsyncMainDemo
             float? TD_10
         );
 
+        /// <summary>
+        /// DWD Database to store the Measurements at.
+        /// </summary>
         private const string ConnectionString = @"Data Source=BYTEFISH\SQLEXPRESS;Integrated Security=true;Initial Catalog=DWD;TrustServerCertificate=Yes";
+
+        /// <summary>
+        /// Data Directory with the TXT and zipped CSV files.
+        /// </summary>
         private const string DataDirectory = @"C:\Users\philipp\Datasets\DWD\";
 
         static async Task Main(string[] args)
@@ -166,73 +174,38 @@ namespace AsyncMainDemo
             await ExecuteNonQueryAsync(ConnectionString, "TRUNCATE TABLE [dbo].[Station]", default);
             await ExecuteNonQueryAsync(ConnectionString, "TRUNCATE TABLE [dbo].[Messwert]", default);
 
+            // Create Indices
+            await ExecuteNonQueryAsync(ConnectionString, @"CREATE CLUSTERED COLUMNSTORE INDEX [CCI_Messwert] ON [dbo].[Messwert] WITH (DROP_EXISTING = ON)", default);
+            await ExecuteNonQueryAsync(ConnectionString, @"CREATE NONCLUSTERED INDEX [IX_Messwert_StationID_MessDatum] ON [dbo].[Messwert]([StationID], [MessDatum]) WITH (DROP_EXISTING = ON)", default);
+
             // Create Stored Procedures
             await ExecuteNonQueryAsync(ConnectionString, @"
-                CREATE OR ALTER PROCEDURE [dbo].[usp_InsertOrUpdateStation]
-                  @Entities [dbo].[udt_StationType] ReadOnly
+                 CREATE OR ALTER PROCEDURE [dbo].[usp_InsertStation]
+                    @Entities [dbo].[udt_StationType] ReadOnly
+                 AS
+                 BEGIN
+    
+                     SET NOCOUNT ON;
+
+                    INSERT INTO [dbo].[Station](StationID, DatumVon, DatumBis, Stationshoehe, GeoBreite, GeoLaenge, Stationsname, Bundesland)
+                    SELECT StationID, DatumVon, DatumBis, Stationshoehe, GeoBreite, GeoLaenge, Stationsname, Bundesland
+                    FROM @Entities;
+
+                 END", default);
+
+            await ExecuteNonQueryAsync(ConnectionString, @"
+                CREATE OR ALTER PROCEDURE [dbo].[usp_InsertMesswert]
+                    @Entities [dbo].[udt_MesswertType] ReadOnly
                 AS
                 BEGIN
     
                     SET NOCOUNT ON;
 
-                    MERGE [dbo].[Station] WITH (SERIALIZABLE) AS TARGET USING @Entities AS SOURCE ON (TARGET.StationID = SOURCE.StationID) 
-                    WHEN MATCHED THEN
-                        UPDATE SET TARGET.StationID = SOURCE.StationID
-                            , TARGET.DatumVon = SOURCE.DatumVon
-                            , TARGET.DatumBis = SOURCE.DatumBis
-                            , TARGET.Stationshoehe = SOURCE.Stationshoehe
-                            , TARGET.GeoBreite = SOURCE.GeoBreite
-                            , TARGET.GeoLaenge = SOURCE.GeoLaenge
-                            , TARGET.Stationsname = SOURCE.Stationsname
-                            , TARGET.Bundesland = SOURCE.Bundesland
-                    WHEN NOT MATCHED BY TARGET THEN
-                        INSERT (StationID, DatumVon, DatumBis, Stationshoehe, GeoBreite, GeoLaenge, Stationsname, Bundesland)
-                        VALUES (SOURCE.StationID, SOURCE.DatumVon, SOURCE.DatumBis, SOURCE.Stationshoehe, SOURCE.GeoBreite, SOURCE.GeoLaenge, SOURCE.Stationsname, SOURCE.Bundesland);
+                    INSERT INTO [dbo].[Messwert](StationID, MessDatum, QN, PP_10, TT_10, TM5_10, RF_10, TD_10)
+                    SELECT StationID, MessDatum, QN, PP_10, TT_10, TM5_10, RF_10, TD_10
+                    FROM @Entities;
 
                 END", default);
-
-            await ExecuteNonQueryAsync(ConnectionString, @"
-                CREATE OR ALTER PROCEDURE [dbo].[usp_InsertOrUpdateMesswert]
-                  @Entities [dbo].[udt_MesswertType] ReadOnly
-                AS
-                BEGIN
-    
-                    SET NOCOUNT ON;
-
-                    MERGE [dbo].[Messwert] WITH (SERIALIZABLE) AS TARGET USING @Entities AS SOURCE ON (TARGET.StationID = SOURCE.StationID AND TARGET.MessDatum = SOURCE.MessDatum) 
-                    WHEN MATCHED THEN
-                        UPDATE SET TARGET.StationID = SOURCE.StationID
-                            , TARGET.MessDatum = SOURCE.MessDatum
-                            , TARGET.QN = SOURCE.QN
-                            , TARGET.PP_10 = SOURCE.PP_10
-                            , TARGET.TT_10 = SOURCE.TT_10
-                            , TARGET.TM5_10 = SOURCE.TM5_10
-                            , TARGET.RF_10 = SOURCE.RF_10
-                            , TARGET.TD_10 = SOURCE.TD_10
-                    WHEN NOT MATCHED BY TARGET THEN
-                        INSERT (StationID, MessDatum, QN, PP_10, TT_10, TM5_10, RF_10, TD_10)
-                        VALUES (SOURCE.StationID, SOURCE.MessDatum, SOURCE.QN, SOURCE.PP_10, SOURCE.TT_10, SOURCE.TM5_10, SOURCE.RF_10, SOURCE.TD_10);
-
-                END", default);
-
-            // Create Indices
-            await ExecuteNonQueryAsync(ConnectionString, @"
-                IF EXISTS (SELECT name FROM sys.indexes 
-                    WHERE name = N'CCI_Messwert_StationID_MessDatum' AND object_id = OBJECT_ID(N'dbo.Messwert'))  
-                BEGIN
-                    DROP INDEX CCI_Messwert_StationID_MessDatum ON [dbo].[Messwert];  
-                END
-
-                CREATE CLUSTERED COLUMNSTORE INDEX [CCI_Messwert_StationID_MessDatum] ON [dbo].[Messwert](StationID, MessDatum) WITH (DROP_EXISTING = OFF)", default);
-
-            await ExecuteNonQueryAsync(ConnectionString, @"
-                IF EXISTS (SELECT name FROM sys.indexes 
-                    WHERE name = N'UX_Messwert_StationID_MessDatum' AND object_id = OBJECT_ID(N'dbo.Messwert'))  
-                BEGIN
-                    DROP INDEX [UX_Messwert_StationID_MessDatum] ON [dbo].[Messwert];  
-                END
-
-                CREATE UNIQUE INDEX [UX_Messwert_StationID_MessDatum] ON [dbo].[Messwert]([StationID], [MessDatum])", default);
 
             // If the Data Directory is empty, then download all data files
             if (Directory.GetFiles(DataDirectory, "*.*").Length == 0)
@@ -264,7 +237,9 @@ namespace AsyncMainDemo
             await Parallel.ForEachAsync(zipFilesToProcess, parallelOptions, async (zipFileToProcess, cancellationToken) =>
             {
                 // Extracts the Measurements from the Zip File:
-                var messwerte = GetMesswerteFromZipFile(zipFileToProcess);
+                var messwerte = GetMesswerteFromZipFile(zipFileToProcess)
+                    // Only use September Measurements
+                    .Where(x => x.MessDatum?.Month == 9);
 
                 // Inserts all Measurements extracted from the Zip File:
                 foreach (var batch in messwerte.Chunk(80000))
@@ -281,7 +256,7 @@ namespace AsyncMainDemo
                         .GroupBy(x => new { x.StationID, x.MessDatum })
                         .Select(x => x.First());
 
-                    await WriteAsync(ConnectionString, data, default);
+                    await WriteAsync(ConnectionString, data, cancellationToken);
                 }
             });
         }
@@ -318,13 +293,12 @@ namespace AsyncMainDemo
                     StationID: columns.StationID.PadLeft(5, '0'),
                     DatumVon: DateTime.ParseExact(columns.DatumVon, "yyyyMMdd", null),
                     DatumBis: DateTime.ParseExact(columns.DatumBis, "yyyyMMdd", null),
-                    Stationshoehe: string.IsNullOrWhiteSpace(columns.Stationshoehe) ? default(float?) : float.Parse(columns.Stationshoehe),
-                    GeoBreite: string.IsNullOrWhiteSpace(columns.GeoBreite) ? default(float?) : float.Parse(columns.GeoBreite),
-                    GeoLaenge: string.IsNullOrWhiteSpace(columns.GeoLaenge) ? default(float?) : float.Parse(columns.GeoLaenge),
-                    Stationsname: string.IsNullOrWhiteSpace(columns.Stationsname) ? default(string?) : columns.Stationsname,
-                    Bundesland: string.IsNullOrWhiteSpace(columns.Bundesland) ? default(string?) : columns.Bundesland
+                    Stationshoehe: string.IsNullOrWhiteSpace(columns.Stationshoehe) ? default(float?) : float.Parse(columns.Stationshoehe, CultureInfo.InvariantCulture),
+                    GeoBreite: string.IsNullOrWhiteSpace(columns.GeoBreite) ? default(float?) : float.Parse(columns.GeoBreite, CultureInfo.InvariantCulture),
+                    GeoLaenge: string.IsNullOrWhiteSpace(columns.GeoLaenge) ? default(float?) : float.Parse(columns.GeoLaenge, CultureInfo.InvariantCulture),
+                    Stationsname: string.IsNullOrWhiteSpace(columns.Stationsname) ? default : columns.Stationsname,
+                    Bundesland: string.IsNullOrWhiteSpace(columns.Bundesland) ? default : columns.Bundesland
                 ));
-
         }
 
         private static IEnumerable<Messwert> GetMesswerteFromZipFile(string zipFilePath)
@@ -343,12 +317,27 @@ namespace AsyncMainDemo
                 .Select(fields => new Messwert(
                     StationID: fields[0].PadLeft(5, '0'),
                     MessDatum: DateTime.ParseExact(fields[1], "yyyyMMddHHmm", null),
-                    QN: int.Parse(fields[2]),
-                    PP_10: string.IsNullOrWhiteSpace(fields[3]) ? null : float.Parse(fields[3]),
-                    TT_10: string.IsNullOrWhiteSpace(fields[4]) ? null : float.Parse(fields[4]),
-                    TM5_10: string.IsNullOrWhiteSpace(fields[5]) ? null : float.Parse(fields[5]),
-                    RF_10: string.IsNullOrWhiteSpace(fields[6]) ? null : float.Parse(fields[6]),
-                    TD_10: string.IsNullOrWhiteSpace(fields[7]) ? null : float.Parse(fields[7])));
+                    QN: int.Parse(fields[2], CultureInfo.InvariantCulture),
+                    PP_10: IsValidMeasurement(fields[3]) ? float.Parse(fields[3], CultureInfo.InvariantCulture) : null,
+                    TT_10: IsValidMeasurement(fields[4]) ? float.Parse(fields[4], CultureInfo.InvariantCulture) : null,
+                    TM5_10: IsValidMeasurement(fields[5]) ? float.Parse(fields[5], CultureInfo.InvariantCulture) : null,
+                    RF_10: IsValidMeasurement(fields[6]) ? float.Parse(fields[6], CultureInfo.InvariantCulture) : null,
+                    TD_10: IsValidMeasurement(fields[7]) ? float.Parse(fields[7], CultureInfo.InvariantCulture) : null));
+        }
+
+        private static bool IsValidMeasurement(string value)
+        {
+            if(string.IsNullOrWhiteSpace(value))
+            {
+                return false;
+            }
+
+            if(string.Equals(value, "-999", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return false;
+            }
+
+            return true;
         }
 
         private static IEnumerable<string> ReadLinesFromZipFile(string zipFilePath)
@@ -374,6 +363,8 @@ namespace AsyncMainDemo
 
         private static async Task WriteAsync(string connectionString, IEnumerable<Station> stations, CancellationToken cancellationToken)
         {
+            var retryLogicProvider = GetExponentialBackoffProvider();
+            
             using (var conn = new SqlConnection(connectionString))
             {
                 // Open the Connection:
@@ -383,8 +374,9 @@ namespace AsyncMainDemo
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
                     // Build the Stored Procedure Command:
-                    cmd.CommandText = "[dbo].[usp_InsertOrUpdateStation]";
+                    cmd.CommandText = "[dbo].[usp_InsertStation]";
                     cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.RetryLogicProvider = retryLogicProvider;
 
                     // Create the TVP:
                     SqlParameter parameter = new SqlParameter();
@@ -434,6 +426,8 @@ namespace AsyncMainDemo
 
         private static async Task WriteAsync(string connectionString, IEnumerable<Messwert> messwerte, CancellationToken cancellationToken)
         {
+            var retryLogicProvider = GetExponentialBackoffProvider();
+
             using (var conn = new SqlConnection(connectionString))
             {
                 // Open the Connection:
@@ -443,8 +437,9 @@ namespace AsyncMainDemo
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
                     // Build the Stored Procedure Command:
-                    cmd.CommandText = "[dbo].[usp_InsertOrUpdateMesswert]";
+                    cmd.CommandText = "[dbo].[usp_InsertMesswert]";
                     cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.RetryLogicProvider = retryLogicProvider;
 
                     // Create the TVP:
                     SqlParameter parameter = new SqlParameter();
@@ -494,15 +489,18 @@ namespace AsyncMainDemo
 
         static async Task ExecuteNonQueryAsync(string connectionString, string sql, CancellationToken cancellationToken)
         {
+            var retryLogicProvider = GetExponentialBackoffProvider();
+
             using (var sqlConnection = new SqlConnection(connectionString))
             {
                 await sqlConnection.OpenAsync(cancellationToken);
 
                 using (var cmd = sqlConnection.CreateCommand())
                 {
+                    cmd.RetryLogicProvider = retryLogicProvider;
                     cmd.CommandType = CommandType.Text;
                     cmd.CommandText = sql;
-
+                    
                     await cmd.ExecuteNonQueryAsync(cancellationToken);
                 }
             }
@@ -526,6 +524,48 @@ namespace AsyncMainDemo
                     existsMode: FtpLocalExists.Overwrite,
                     token: cancellationToken);
             }
+        }
+
+        /// <summary>
+        /// As a gentle reminder, the following list of transient errors are handled by the RetryLogic if the transient errors are <see cref="null"/>:
+        /// 
+        ///     1204,   // The instance of the SQL Server Database Engine cannot obtain a LOCK resource at this time. Rerun your statement when there are fewer active users. Ask the database administrator to check the lock and memory configuration for this instance, or to check for long-running transactions.
+        ///     1205,   // Transaction (Process ID) was deadlocked on resources with another process and has been chosen as the deadlock victim. Rerun the transaction
+        ///     1222,   // Lock request time out period exceeded.
+        ///     49918,  // Cannot process request. Not enough resources to process request.
+        ///     49919,  // Cannot process create or update request. Too many create or update operations in progress for subscription "%ld".
+        ///     49920,  // Cannot process request. Too many operations in progress for subscription "%ld".
+        ///     4060,   // Cannot open database "%.*ls" requested by the login. The login failed.
+        ///     4221,   // Login to read-secondary failed due to long wait on 'HADR_DATABASE_WAIT_FOR_TRANSITION_TO_VERSIONING'. The replica is not available for login because row versions are missing for transactions that were in-flight when the replica was recycled. The issue can be resolved by rolling back or committing the active transactions on the primary replica. Occurrences of this condition can be minimized by avoiding long write transactions on the primary.
+        ///     40143,  // The service has encountered an error processing your request. Please try again.
+        ///     40613,  // Database '%.*ls' on server '%.*ls' is not currently available. Please retry the connection later. If the problem persists, contact customer support, and provide them the session tracing ID of '%.*ls'.
+        ///     40501,  // The service is currently busy. Retry the request after 10 seconds. Incident ID: %ls. Code: %d.
+        ///     40540,  // The service has encountered an error processing your request. Please try again.
+        ///     40197,  // The service has encountered an error processing your request. Please try again. Error code %d.
+        ///     42108,  // Can not connect to the SQL pool since it is paused. Please resume the SQL pool and try again.
+        ///     42109,  // The SQL pool is warming up. Please try again.
+        ///     10929,  // Resource ID: %d. The %s minimum guarantee is %d, maximum limit is %d and the current usage for the database is %d. However, the server is currently too busy to support requests greater than %d for this database. For more information, see http://go.microsoft.com/fwlink/?LinkId=267637. Otherwise, please try again later.
+        ///     10928,  // Resource ID: %d. The %s limit for the database is %d and has been reached. For more information, see http://go.microsoft.com/fwlink/?LinkId=267637.
+        ///     10060,  // An error has occurred while establishing a connection to the server. When connecting to SQL Server, this failure may be caused by the fact that under the default settings SQL Server does not allow remote connections. (provider: TCP Provider, error: 0 - A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.) (Microsoft SQL Server, Error: 10060)
+        ///     997,    // A connection was successfully established with the server, but then an error occurred during the login process. (provider: Named Pipes Provider, error: 0 - Overlapped I/O operation is in progress)
+        ///     233     // A connection was successfully established with the server, but then an error occurred during the login process. (provider: Shared Memory Provider, error: 0 - No process is on the other end of the pipe.) (Microsoft SQL Server, Error: 233)
+        /// </summary>
+        private static SqlRetryLogicBaseProvider GetExponentialBackoffProvider(int numberOfTries = 5, int deltaTimeInSeconds = 1, int maxTimeIntervalInSeconds = 20, IEnumerable<int>? transientErrors = null)
+        {
+            // Define the retry logic parameters
+            var options = new SqlRetryLogicOption()
+            {
+                // Tries 5 times before throwing an exception
+                NumberOfTries = numberOfTries,
+                // Preferred gap time to delay before retry
+                DeltaTime = TimeSpan.FromSeconds(deltaTimeInSeconds),
+                // Maximum gap time for each delay time before retry
+                MaxTimeInterval = TimeSpan.FromSeconds(maxTimeIntervalInSeconds),
+                // List of Transient Errors to handle
+                TransientErrors = transientErrors
+            };
+
+            return SqlConfigurableRetryFactory.CreateExponentialRetryProvider(options);
         }
     }
 }
